c6e5f8cc93f208e6770b8c76045110bd5eba8a5d
ITR <ITR@live.no>
2024-01-11 22:24:31 +0100

    Initial Commit

---
+++ CobaltCoreEasyLoader/EasyLoader.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Extensions.Logging;
+using Nanoray.PluginManager;
+using Nickel;
+using OneOf;
+using OneOf.Types;
+
+namespace CobaltCoreEasyLoader;
+
+public class EasyLoader(
+    IPluginLoader<IModManifest, Mod> pluginLoader,
+    Func<IPluginPackage<IModManifest>, ILogger> modLoggerGetter,
+    Func<IPluginPackage<IModManifest>, IModHelper> modHelperGetter,
+    ExtendableAssemblyPluginLoaderParameterInjector<IModManifest> parameterInjector
+)
+    : IPluginLoader<IModManifest, Mod>
+{
+    /*  ModFolder
+     *      Sprites/
+     *          Character/
+     *              CharA
+     *              CharB
+     *          Cards
+     *              CardA
+     *              CardB
+     *          ShipParts
+     *          Artifacts
+     *      Localization/
+     *      SomeAssembly.dll
+     */
+
+    public OneOf<Yes, No, Error<string>> CanLoadPlugin(IPluginPackage<IModManifest> package)
+    {
+        return package.Manifest.ModType == "EasyLoader" ? pluginLoader.CanLoadPlugin(package) : new No();
+    }
+
+    public OneOf<Mod, Error<string>> LoadPlugin(IPluginPackage<IModManifest> package)
+    {
+        var root = package.PackageRoot;
+        var logger = modLoggerGetter(package);
+        var helper = modHelperGetter(package);
+
+        var sprites = MaybeLoadSprites(logger, root.GetRelativeDirectory("Sprites"), helper.Content.Sprites);
+
+        var injector = new CompoundAssemblyPluginLoaderParameterInjector<IModManifest>(
+            new ValueAssemblyPluginLoaderParameterInjector<IModManifest, Dictionary<string, ISpriteEntry>>(sprites)
+        );
+
+        parameterInjector.RegisterParameterInjector(injector);
+        try
+        {
+            return pluginLoader.LoadPlugin(package);
+        }
+        finally
+        {
+            parameterInjector.UnregisterParameterInjector(injector);
+        }
+    }
+
+    private Dictionary<string, ISpriteEntry> MaybeLoadSprites(
+        ILogger logger,
+        IDirectoryInfo spriteRoot,
+        IModSprites modSprites
+    )
+    {
+        if (!spriteRoot.Exists)
+        {
+            logger.LogWarning("Failed to find Sprites directory");
+            return new Dictionary<string, ISpriteEntry>();
+        }
+
+        var sprites = new Dictionary<string, ISpriteEntry>();
+        foreach (var fileInfo in spriteRoot.GetFilesRecursively())
+        {
+            var ext = Path.GetExtension(fileInfo.Name);
+            if (ext != ".png")
+            {
+                logger.LogWarning("Found non png file in Sprites directory: {}", fileInfo.FullName);
+                continue;
+            }
+
+            var fullName = Path
+                .GetRelativePath(spriteRoot.FullName, fileInfo.FullName)[..^4]
+                .ToLower()
+                .Replace(Path.DirectorySeparatorChar, '/');
+            var entry = modSprites.RegisterSprite(fullName, fileInfo);
+            sprites.Add(fullName, entry);
+        }
+
+        logger.LogInformation("Successfully loaded {} sprites", sprites.Count);
+        return sprites;
+    }
+}
---
+++ CobaltCoreEasyLoader/EasyLoaderMod.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace CobaltCoreEasyLoader;
+
+public class EasyLoaderMod : Mod
+{
+    public EasyLoaderMod(
+        ExtendablePluginLoader<IModManifest, Mod> extendablePluginLoader,
+        Func<IPluginPackage<IModManifest>, ILogger> modLoggerGetter,
+        Func<IPluginPackage<IModManifest>, IModHelper> modHelperGetter,
+        
+        IAssemblyPluginLoaderLoadContextProvider<IAssemblyModManifest> loadContextProvider,
+        ExtendableAssemblyPluginLoaderParameterInjector<IModManifest> parameterInjector,
+        IAssemblyEditor? assemblyEditor
+    )
+    {
+        extendablePluginLoader.RegisterPluginLoader(
+            new EasyLoader(
+                new SpecializedConvertingManifestPluginLoader<IAssemblyModManifest, IModManifest, Mod>(
+                    new AssemblyPluginLoader<IAssemblyModManifest, Mod, Mod>(
+                        requiredPluginDataProvider: p => new AssemblyPluginLoaderRequiredPluginData
+                        {
+                            UniqueName = p.Manifest.UniqueName,
+                            EntryPointAssembly = p.Manifest.EntryPointAssembly,
+                            EntryPointType = p.Manifest.EntryPointType
+                        },
+                        loadContextProvider: loadContextProvider,
+                        partAssembler: new SingleAssemblyPluginPartAssembler<IAssemblyModManifest, Mod>(),
+                        parameterInjector: parameterInjector,
+                        assemblyEditor: assemblyEditor
+                    ),
+                    m => m.AsAssemblyModManifest()
+                ),
+                modLoggerGetter,
+                modHelperGetter,
+                parameterInjector
+            )
+        );
+    }
+}
